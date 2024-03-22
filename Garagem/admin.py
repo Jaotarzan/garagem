0515da88c4fc7029bb058313a14e2fe352a8f30d
Jaotarzan <joaovssouza59@gmail.com>
2024-03-22 17:35:46 -0300

    Criação dos modelos cor, acessório, veiculo

--- Garagem/admin.py
+++ Garagem/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
-from .models import Categoria, Marca
+from .models import Acessório, Categoria, Cor, Marca, Veículo
 
 # Register your models here.
 admin.site.register(Marca)
 admin.site.register(Categoria)
+admin.site.register(Acessório)
+admin.site.register(Cor)
+admin.site.register(Veículo)
--- Garagem/models.py
+++ Garagem/models.py
@@ -13,3 +13,26 @@ class Categoria(models.Model):
 
     def __str__(self):
         return self.descricao
+    
+class Acessório(models.Model):
+    descricao = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.descricao
+    
+class Cor(models.Model):
+    descricao = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.descricao
+    
+class Veículo(models.Model):
+    modelo = models.CharField(max_length=100)
+    marca = models.ForeignKey(Marca, on_delete=models.PROTECT, related_name="veiculo")
+    categoria = models.ForeignKey(Categoria, on_delete=models.PROTECT, related_name="veiculo")
+    cor = models.ForeignKey(Cor, on_delete=models.PROTECT, related_name="veiculo")
+    ano = models.IntegerField(null=True, blank=True, default=0)
+    preco = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True, default=0)
+
+    def __str__(self) -> str:
+        return f"{self.marca} {self.modelo} {self.ano} {self.cor}"
